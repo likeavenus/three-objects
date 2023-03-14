2cd4792a27b8864a1cf8ec9ecd8a92020eaacdce
Rafael <rafael_guliev94@list.ru>
2023-03-14 14:56:35 +0300

    refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     assetsInclude: ['**/*.gltf', '**/*.3DS', '**/*.mp3', '**/*.png', '**/*.bin'],
-    // base: '/three-objects/',
+    base: '/three-objects/',
     server: {
         watch: {
             usePolling: true,
